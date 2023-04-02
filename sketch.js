e55248a03f18344386b972a2eb433b0468828e46
lekenplass <129627781+lekenplass@users.noreply.github.com>
2023-04-02 10:20:21 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,41 +1,36 @@
 let t = 0;
 let img;
-function preload(){
-  img=loadImage("sopp.jpg");
+function preload() {
+  img = loadImage("sopp.jpg");
 }
 function setup() {
   pixelDensity(1);
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(0,255,0);
-  
+  background(0, 255, 0);
+
   let tw = 50; //tile width
   let th = 50; //tile height
-beginShape();
-  for (let x= 0 ; x<width ; x+=tw){
-    for (let y= 0 ; y<height ; y+=tw){
-      
-      let nw = map(noise(x,y,t), 0,1,0, tw);
-      let nh = map(noise(x,y,t), 0,1,0, th);
-      
-      
-    image(img,x,y,tw,th, x, y, nw+tw, nh+th);
+  beginShape();
+  for (let x = 0; x < canvas.width; x += tw) {
+    for (let y = 0; y < canvas.height; y += tw) {
+      let nw = map(noise(x, y, t), 0, 1, 0, tw);
+      let nh = map(noise(x, y, t), 0, 1, 0, th);
+
+      image(img, x, y, tw, th, x, y, nw + tw, nh + th);
+    }
+    endShape();
+    t += 0.0002;
   }
-  endShape();
-    t+=0.0002;
-    
 }
-}
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
+
 function mousePressed() {
   let fs = fullscreen();
   fullscreen(!fs);
 }
-function touchStarted () {
+function touchStarted() {
   var fs = fullscreen();
   if (!fs) {
     fullscreen(true);
@@ -50,6 +45,6 @@ function windowResized() {
 /* prevents the mobile browser from processing some default
  * touch events, like swiping left for "back" or scrolling the page.
  */
-document.ontouchmove = function(event) {
-    event.preventDefault();
+document.ontouchmove = function (event) {
+  event.preventDefault();
 };
