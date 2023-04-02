70dae810ea51a2ae68311a8a95690628fbaa49dc
lekenplass <129627781+lekenplass@users.noreply.github.com>
2023-04-02 10:11:50 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+let t = 0;
+let img;
+function preload(){
+  img=loadImage("sopp.jpg");
+}
+function setup() {
+  pixelDensity(1);
+  createCanvas(windowWidth,windowHeight);
+}
+
+function draw() {
+  background(0,255,0);
+  
+  let tw = 50; //tile width
+  let th = 50; //tile height
+beginShape();
+  for (let x= 0 ; x<width ; x+=tw){
+    for (let y= 0 ; y<height ; y+=tw){
+      
+      let nw = map(noise(x,y,t), 0,1,0, tw);
+      let nh = map(noise(x,y,t), 0,1,0, th);
+      
+      
+    image(img,x,y,tw,th, x, y, nw+tw, nh+th);
+  }
+  endShape();
+    t+=0.0002;
+    
+}
+}
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+function mousePressed() {
+  let fs = fullscreen();
+  fullscreen(!fs);
+}
+function touchStarted () {
+  var fs = fullscreen();
+  if (!fs) {
+    fullscreen(true);
+  }
+}
+
+/* full screening will change the size of the canvas */
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+/* prevents the mobile browser from processing some default
+ * touch events, like swiping left for "back" or scrolling the page.
+ */
+document.ontouchmove = function(event) {
+    event.preventDefault();
+};
